2d90c09b5156c205c35e8cde2f17b2699d0a1a74
Sukrit Khera <sukrit007@gmail.com>
2015-12-02 14:26:12 -0700

    Use hardcoded key (by default)

--- src/main/java/com/meltmedia/pgp/samples/KeyManagement.java
+++ src/main/java/com/meltmedia/pgp/samples/KeyManagement.java
@@ -133,6 +133,56 @@ public class KeyManagement {
   }
 
   public PGPPublicKey fetchKey(String keyId) throws UnirestException, IOException, PGPException {
+    // Uncomment below to fetch from key server rather using hardcoded value.
+    //String rawKey = fetchKeyRaw(keyId);
+    String rawKey =  "-----BEGIN PGP PUBLIC KEY BLOCK-----\n" +
+        "Version: SKS 1.1.5\n" +
+        "Comment: Hostname: pgp.mit.edu\n" +
+        "\n" +
+        "mQINBFZLkWUBEADZYqwaNj4JQLsR/1fTcJQaxRlvmeLViRfRwndkaJm9N6uVOSTQpdS4RBFo\n" +
+        "ZYUYy+cMjLK1hBeoyqHZwEOrI6xuzi2bsGJu8Yj/cTIIzyNOOsGtYxSLPFcKi99vPScfjTC8\n" +
+        "nZ7U/3MbUEnJ+x+TeX2CwIvwK7MW7Kcgn2airv0D2Es1oqjGiFA0DnFhP3BI/TM6qmhOlJ4j\n" +
+        "YXn6/0lyW//18i6k9rl6cKmq+I1hEwPtJpnGMwUudfrggGD9LXvN1qmhdbcnilj6c5LPmMGr\n" +
+        "Ggio7B0zlzgp/Z0lin4JGslBC/61SHKsXgHBnAPcBSHGIO1Zus7XmKxjx5SgihknuQZ6DATe\n" +
+        "Vs2IjrkTi3P3n0tbSs6RljKawWTVzcfJJ1Nj2pnB/Gc6S4uw7r6dYexfkGMML7Z1ylN06UQx\n" +
+        "MezYV1LToilpnMCK6eH6NG3dHjOctWr9hz0gll0ggQNkA/JkSusqsNnp/dc2qjerp5j1sCrJ\n" +
+        "zHs0+Gdx9W0lvwAzcrpmGO4/oCHUt1+SwH7aJYZZeHS05Ynohlxfp3vkyZBTqucR7qUD1Xfc\n" +
+        "EGp+3tAdwTs5ZZtxkF/3qhcdgfn4qvVVTOSZi0rg0oSm5jpBtVVd3qkwEQRZJiPSAQX/bl3J\n" +
+        "s3D8oPg25M4bYusrq9TMXXaOqzKB0ReQBCDEKp00OtKwN00OCQARAQABtFVQaWNhcmQgRGV2\n" +
+        "ZWxvcG1lbnQgKFB1YmxpYyBLZXkgZm9yIGVuY3J5cHRpbmcgd2l0aCBwaWNhcmQpIDxwaWNh\n" +
+        "cmQtZGV2QG1lbHRtZWRpYS5jb20+iQI3BBMBCgAhBQJWS5FlAhsDBQsJCAcDBRUKCQgLBRYC\n" +
+        "AwEAAh4BAheAAAoJEFVIpZdYI6uZW8AQAL+bdUIs/GA5GMI29lRqTi86WOFWLAk3hXDhjijr\n" +
+        "rZNAVgtHvF2Zvfui4XtNXG57u5XqbaeqJ6kSkOZWA/X2nJK9zU33e1ZbYWWd0AVvSQVL67NF\n" +
+        "zUOEzHZT6mHdwOdbO/PtvmkP2d9J40PdAJVixYVqJ7/oNIYjodpnPlpFocqJVKbcWdsKrF09\n" +
+        "KCzTfjp3wb5ooIUg2ygxWcQKllijFOgXVdC/vjCAqZUkMv4Zofaz56lZXEfnjLJZajk3zKRh\n" +
+        "cL+wcAw0Ex0w0Sla26h9f4CewdGRKZf/kvmsbKTuw2Cxz5pMhgI7Fj82VtBNqEH/dEes2x9c\n" +
+        "yFp5MGK/c2DTZo9qKaJPoFjvNK9Xmlsy9RG87t2jq1zTwQYMeGEztAkWPWxPwtS7uHDL7vb9\n" +
+        "LhBGBSXfyiJV9HdxaieQ8Lfz5DnskGwNNGh05ZXBL7ecTc4O6bqroCLg9bn1xgUFD8GXUZ0R\n" +
+        "qgwwKcIfvWPb+8H04WBYJ0L7H9o3AQweNS64iLKdNbCCd6iPDESFeEQzM+Uss4y58qOMwJxs\n" +
+        "aGplHsYq7lyreKUd/haXZKpWvp2QVV1g7gRL/LVRyABGxaDfTRN2Tlev/wNinwWxIpmPOXGN\n" +
+        "KrxnBHYzlgPBe+17UdgAKlSM1KQfgj4FxxjAEPemoNGK/ymOc4KsdkNmD8K6plD+DnEHuQIN\n" +
+        "BFZL/isBEADFeLnhh9PmZOL7quoir+h8iGfXUcdwPGkBk6rtjWdInp1BKZNholEmFTK7rPsU\n" +
+        "kp9WPwEJUw/r0ubZTVNO9JyQ+AaAbODSdXJFAgs+zTlt7vBtHvUyFgKCNPm0xkTQba7wCCES\n" +
+        "W3k1xGqNfNWDATL5UNE6d5R8NLr9cw/IIRIxwm0qE5gFO3gM8OzsXnJDRFzwLFa+NVpSdG4U\n" +
+        "bSL1zkfVAsRxt9VCrKA7cae+NJ3memHHIQJrvXrsCUCZZA24BBlzz4WVar/2PIS7x+9PeH/g\n" +
+        "FuJG+hETVFnmCJqsMlAq8fBm/1lZ6UCv4PUjAS6y1jO3DtX/oG3owQkAJBCLSJdaXsI1NWxR\n" +
+        "v5ecgBnDdZ9Dz4VZVg+WF4BuZ5sbd+S42s4WWSCGUlxU7Ekyvq4ZJjFExh/mSdVGhcCq4+dq\n" +
+        "M/uIPongI5gVv6J8613Z+J47CMjetYteAS8keCLs+h+GAqsXiUOewXQDx5ECc1Uh5cVapTk9\n" +
+        "YZw6k9Mt2oo5MFSovpTTBFJixtfNp69H14Pgx0h8XGBbbTdrLPGf1W89vTrPBQGFkjSoGsNB\n" +
+        "GHp0ksExC4hfF3k8e52f1IyIi7pdTuAiT/yVYkiwe99nmb8qQUR40CMopfJ0aOIV8v2szzrd\n" +
+        "unlh1tokQVgR1JukMZZ+wAkZo9CgweY3a+5tK9NOlmcq8wARAQABiQIlBBgBCgAPBQJWS/4r\n" +
+        "AhsMBQkDw7iAAAoJEFVIpZdYI6uZ6p4QAMAjMWmpS+S9hT7YVXMOakQF3Vqv/4fvyqdZ8Ab1\n" +
+        "4iHiWFgNz/ONdXi/ls1TwylECtYNFo8XN6cZlyigZeYjO812NUREJGWvCU74MIDnTwoqwmvL\n" +
+        "sqyBlWBujTMqBRpbBEweE/W1olD8rxurfmTCJJB7Z1ncfxk2Uf94XNpB54S52HBeLK4HxAUT\n" +
+        "7By4gZKVJB0qDhuiKL6kGGMWGrJRFMA6lYyQzoFAq/cDSkAp8b2NnkS/4BpvNwhq1zVJsSBw\n" +
+        "V2DhN6UTfWazQjSrULlbU0wQlYX70XUbL7s2x1MzGFWpoE9rGRe0p1dVmz25qTh24Oz97E86\n" +
+        "vXo6y9XBMkDbBazGSw/VAWb7GC96RSPxAr6Z3c9xBniCDuMCLT2kYiEVxgSz5KQNs11CCR5m\n" +
+        "Q1+fQqC+HNacDJtotE9i4RzYKUXilRQBQTXzLSZQcghvmbjbn0KqMUWDlXm8YEhHZnvFydtt\n" +
+        "0645DsgCk8PWlvS32xREvDnxOlsxF4r5DEKl5VY1mE+35yRyiAuu1g/5XuRLOeqa4Gc9hXaM\n" +
+        "jQju5rk5c5ElVyRuW7e0NEEcWgDZPmJTTZk3kSlI8jmKJrnsv7FhGAmGt8GKQaiNrrqs91ox\n" +
+        "3ji97wViCPcRVz1rA1LcecpQw02kZm4JZopVwiKrWm7o+7vI2UYUIeMS3ccH9k3TshHE\n" +
+        "=cilC\n" +
+        "-----END PGP PUBLIC KEY BLOCK-----";
     return readPublicKey(fetchKeyRaw(keyId));
   }
 
